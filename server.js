04109b25c209b05a5e17c0afb33c62111b7152c5
chrisgass <gassachr@students.zhaw.ch>
2025-05-25 00:42:36 +0200

    automatisches deployment via main branch

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('(-: Hello FS2025 DevOps Course and now for a docker deployment with github actions! :-)');
+  res.send('(-: Hello FS2025 DevOps Course and now for a docker deployment with github actions, that runs automatically in main! :-)');
 });
 
 app.listen(PORT, HOST);
